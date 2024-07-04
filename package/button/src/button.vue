f7cf5ca69e6764160646a4e1c428c22ef5a5058a
yuandabo <540496776@qq.com>
2024-07-04 20:01:26 +0800

    feat: lint

--- package/button/src/button.vue
+++ package/button/src/button.vue
@@ -18,3 +18,25 @@ function clickHandler() {
     <slot></slot>
   </button>
 </template>
+
+<style lang="scss">
+.testClass {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  font-size: 1rem;
+  font-weight: 500;
+  line-height: 1.5;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  user-select: none;
+  border: 1px solid transparent;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  color: #212529;
+  background-color: #e9ecef;
+}
+</style>
+
